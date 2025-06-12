07b9e742b1bca43f024f3ede1bbcf9884aa3fc06
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2025-06-12 12:51:05 +0300

    from req/rep to pub/sub

--- Test_work_client/client/client/client.cpp
+++ Test_work_client/client/client/client.cpp
@@ -7,17 +7,17 @@ int main()
 {
     static const int k_number_of_threads = 2;
     zmq::context_t context(k_number_of_threads);
-    zmq::socket_t socket(context, zmq::socket_type::req);
-    socket.connect("tcp://localhost:5555"); // Заменить позже на айпишник сервера
+    zmq::socket_t subscriber(context, zmq::socket_type::sub);
+    subscriber.connect("tcp://localhost:5555"); // Заменить позже на айпишник сервера
 
-    for (int i = 0; i < 10; ++i)
+    subscriber.set(zmq::sockopt::subscribe, "");
+
+    while (true)
     {
-        std::string msg = "Nya from client";
-        socket.send(zmq::buffer(msg), zmq::send_flags::none);
+        zmq::message_t zmq_msg;
+        subscriber.recv(zmq_msg, zmq::recv_flags::none);
 
-        zmq::message_t reply;
-        socket.recv(reply, zmq::recv_flags::none);
-
-        std::cout << "From server: " << reply.to_string() << "\n";
+        std::string received_msg(static_cast<char*>(zmq_msg.data()), zmq_msg.size());
+        std::cout << "From server: " << received_msg << "\n";
     }
 }
--- Test_work_server/server/server/server.cpp
+++ Test_work_server/server/server/server.cpp
@@ -2,22 +2,25 @@
 #include <iostream>
 #include <string.h>
 #include <assert.h>
+#include <thread>
+#include <chrono>
+
+#include "Lobby.h"
 
 int main()
 {
     static const int k_number_of_threads = 2;
     zmq::context_t context(k_number_of_threads);
-    zmq::socket_t socket(context, zmq::socket_type::rep);
-    socket.bind("tcp://*:5555");
+    zmq::socket_t publisher(context, zmq::socket_type::pub);
+    publisher.bind("tcp://*:5555");
 
     while (true)
     {
-        zmq::message_t request;
+        std::string nya = "Nya";
+        zmq::message_t zmq_msg(nya.begin(), nya.end());
+        publisher.send(zmq_msg, zmq::send_flags::none);
 
-        auto result = socket.recv(request, zmq::recv_flags::none);
-        std::cout << request.to_string() << "\n";
-
-        std::string reply = "Nya";
-        socket.send(zmq::buffer(reply), zmq::send_flags::none);
+        std::cout << "Sent: " << nya << "\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
