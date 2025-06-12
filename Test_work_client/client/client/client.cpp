6a68926dc279ca83b9986dc8fb7e74a361aba846
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2025-06-12 12:30:19 +0300

    upd

--- Test_work_client/client/client/client.cpp
+++ Test_work_client/client/client/client.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <zmq.hpp>
+#include <iostream>
+#include <string.h>
+#include <assert.h>
+
+int main()
+{
+    static const int k_number_of_threads = 2;
+    zmq::context_t context(k_number_of_threads);
+    zmq::socket_t socket(context, zmq::socket_type::req);
+    socket.connect("tcp://localhost:5555"); // Заменить позже на айпишник сервера
+
+    for (int i = 0; i < 10; ++i)
+    {
+        std::string msg = "Nya from client";
+        socket.send(zmq::buffer(msg), zmq::send_flags::none);
+
+        zmq::message_t reply;
+        socket.recv(reply, zmq::recv_flags::none);
+
+        std::cout << "From server: " << reply.to_string() << "\n";
+    }
+}
--- Test_work_server/server/server/server.cpp
+++ Test_work_server/server/server/server.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <zmq.hpp>
+#include <iostream>
+#include <string.h>
+#include <assert.h>
+
+int main()
+{
+    static const int k_number_of_threads = 2;
+    zmq::context_t context(k_number_of_threads);
+    zmq::socket_t socket(context, zmq::socket_type::rep);
+    socket.bind("tcp://*:5555");
+
+    while (true)
+    {
+        zmq::message_t request;
+
+        auto result = socket.recv(request, zmq::recv_flags::none);
+        std::cout << request.to_string() << "\n";
+
+        std::string reply = "Nya";
+        socket.send(zmq::buffer(reply), zmq::send_flags::none);
+    }
+}
